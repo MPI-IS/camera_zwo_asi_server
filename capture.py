06fe2fbd0fc11e92a7d976fd07d9be57ae051fe8
Vincent Berenz <vberenz@tue.mpg.de>
2024-11-08 22:55:42 +0100

    dev

--- capture.py
+++ capture.py
@@ -7,6 +7,35 @@ import cv2
 from dataclasses import dataclass
 import numpy as np
 from PIL import Image
+import camera_zwo_asi as zwo
+import nightskycam_focus as nf
+
+
+class FocusAdapter:
+    initialized: bool = False
+
+    @classmethod
+    def init(cls) -> None:
+        if not cls.initialized:
+            nf.adapter.init_adapter()
+            cls.initialized = True
+
+    @classmethod
+    def close(cls) -> None:
+        if cls.initialized:
+            nf.adapter.idle_adapter()
+
+    @classmethod
+    def focus(cls, focus: Optional[int]) -> None:
+        if focus is not None:
+            cls.init()
+            nf.adapter.set_focus(focus)
+
+    @classmethod
+    def aperture(cls, aperture: Optional[int]) -> None:
+        if aperture is not None:
+            cls.init()
+            nf.adapter.set_aperture(aperture)
 
 
 class CameraType(Enum):
@@ -59,6 +88,18 @@ def _webcam_capture() -> np.ndarray:
         if not ret:
             raise RuntimeError("Failed to capture image with the webcam")
     return frame
+
+
+def _zwo_asi_capture(camera_config: CameraConfig) -> np.ndarray:
+    camera = zwo.Camera(0)
+    camera.set_control("Exposure", camera_config.exposure)
+    camera.set_control("Gain", camera_config.gain)
+    roi = camera.get_roi()
+    roi.type = zwo.ImageType.rgb24
+    camera.set_roi(roi)
+    FocusAdapter.focus(camera_config.focus)
+    FocusAdapter.aperture(camera_config.aperture)
+    return camera.capture().get_image()
 
 
 def create_thumbnail(
